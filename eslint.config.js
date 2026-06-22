622d0643513481ad12a8d9c1f36f35ff5ea67d5b
Adedeji Mustapha <adedeji.mustapha.2018@gmail.com>
2026-06-22 20:36:26 +0100

    refactor: migrate to pure flat config and fix linting circularity

--- eslint.config.js
+++ eslint.config.js
@@ -1,24 +1,39 @@
-const { FlatCompat } = require("@eslint/eslintrc");
+import js from '@eslint/js';
+import ts from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import reactHooks from 'eslint-plugin-react-hooks';
+import nextPlugin from '@next/eslint-plugin-next';
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-module.exports = [
-  ...compat.config({
-    extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
-    plugins: ["@typescript-eslint"],
-    rules: {
-      "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/no-unused-vars": [
-        "warn",
-        {
-          argsIgnorePattern: "^_",
-          varsIgnorePattern: "^_",
-        },
-      ],
-      "react/no-unescaped-entities": "warn",
-      "react-hooks/exhaustive-deps": "warn",
+export default [
+  js.configs.recommended,
+  {
+    ignores: ['.next/**', 'node_modules/**', 'dist/**'],
+  },
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    plugins: {
+      '@typescript-eslint': ts,
+      'react-hooks': reactHooks,
+      '@next/next': nextPlugin,
     },
-  }),
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+        ecmaVersion: 2022,
+        sourceType: 'module',
+      },
+    },
+    rules: {
+      ...ts.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      ...nextPlugin.configs.recommended.rules,
+      ...nextPlugin.configs['core-web-vitals'].rules,
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
+    },
+  },
 ];
